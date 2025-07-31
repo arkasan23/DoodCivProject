ab9811f6fe46ca8e4d74ca5ff16a1a431b2e59de
Arsalan <arkasan23@gmail.com>
2025-07-30 23:07:53 -0400

    menu music test

--- app/public/menu.js
+++ app/public/menu.js
@@ -3,31 +3,35 @@ export class Menu extends Phaser.Scene {
     super("menu");
   }
 
+  preload() {
+    this.load.audio("menuMusic", "assets/test.mp3");
+  }
+
   create() {
     console.log("does this create?");
     this.menu = this.add.group();
 
-    let title = this.add.text(
-      -500,
-      this.scale.height / 4,
-      "Placeholder game title",
-      {
-        fontFamily: '"JetBrains Mono", monospace',
-        fontSize: "48px",
-        color: "#ffffff",
-      },
-    );
+    let music = this.sound.add("menuMusic", {
+      loop: true,
+      volume: 0.5,
+    });
 
-    let subTitle = this.add.text(
-      -500,
-      this.scale.height / 3.35,
-      "A game by the Doods",
-      {
-        fontFamily: '"JetBrains Mono", monospace',
-        fontSize: "20px",
-        color: "#ffffff",
-      },
-    );
+    music.play();
+
+    const centerX = this.scale.width / 2;
+    const centerY = this.scale.height / 2;
+
+    let title = this.add.text(-500, centerY - 200, "Placeholder game title", {
+      fontFamily: '"JetBrains Mono", monospace',
+      fontSize: "48px",
+      color: "#ffffff",
+    });
+
+    let subTitle = this.add.text(-500, centerY - 150, "A game by the Doods", {
+      fontFamily: '"JetBrains Mono", monospace',
+      fontSize: "20px",
+      color: "#ffffff",
+    });
 
     title.setOrigin(0.5);
     subTitle.setOrigin(0.5);
@@ -36,7 +40,7 @@ export class Menu extends Phaser.Scene {
 
     this.tweens.add({
       targets: title,
-      x: this.scale.width / 4,
+      x: centerX - 400,
       ease: "Sine.easeOut",
       duration: 1000,
       delay: 200,
@@ -44,7 +48,7 @@ export class Menu extends Phaser.Scene {
 
     this.tweens.add({
       targets: subTitle,
-      x: this.scale.width / 5.5,
+      x: centerX - 400,
       ease: "Sine.easeOut",
       duration: 1000,
       delay: 600,
