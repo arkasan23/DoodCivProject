dd3c7d23aad5638c650ca82f26413f6bb838260c
StanleyGian <sg3759@drexel.edu>
2025-08-06 16:11:39 -0400

    level1.json should be viewable

--- app/public/gameScene.js
+++ app/public/gameScene.js
@@ -5,26 +5,32 @@ export class GameScene extends Phaser.Scene {
     super("game");
   }
 
+  preload() {
+    this.load.json("level1", "assets/levels/level1.json");
+  }
+
   create() {
-    //TODO maybe have each level be a json file that contains title data???
+    const levelData = this.cache.json.get("level1");
 
     this.tiles = [];
     const radius = 30;
-    const cols = 10;
-    const rows = 10;
     const hexWidth = Math.sqrt(3) * radius;
     const hexHeight = 2 * radius;
+
+    const cols = levelData.cols;
+    const rows = levelData.rows;
+
     const gridPixelWidth = hexWidth * cols + hexWidth / 2;
     const gridPixelHeight = hexHeight * 0.75 * rows + hexHeight / 4;
 
     const offsetX = (this.sys.game.config.width - gridPixelWidth) / 2;
     const offsetY = (this.sys.game.config.height - gridPixelHeight) / 2;
 
-    for (let r = 0; r < rows; r++) {
-      for (let q = 0; q < cols; q++) {
-        const tile = new Tile(this, q, r, offsetX, offsetY, 0x88cc88);
-        this.tiles.push(tile);
-      }
+    for (const tileData of levelData.tiles) {
+      const { q, r, color } = tileData;
+      const tileColor = parseInt(color); // Convert from "0x88cc88" string to number
+      const tile = new Tile(this, q, r, offsetX, offsetY, tileColor);
+      this.tiles.push(tile);
     }
   }
 }
